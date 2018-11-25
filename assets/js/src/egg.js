e67e2ffbc9184680233a7545d31a908ae29ce4dc
Weixiao <rjxyweixiao@163.com>
2018-11-26 00:10:29 +0800

    IDEA 提交

---
+++ assets/js/src/egg.js
@@ -0,0 +1,32 @@
+/**
+ * @Description : 神秘彩蛋
+ * @author Weixiao
+ * @Date 2018-10-27 21:04:58
+ * @email rjxyweixiao@163.com
+ */
+
+var input = new Array(30);
+$(document).keydown(function(e){
+    input.push(e.key);
+    input.shift();
+    var inputString = input.toString().replace(/,/g,"");
+    if(inputString.indexOf("EnterhideblogcontentEnter") !== -1){
+        hideContent();
+    }
+    if(inputString.indexOf("EntershowblogcontentEnter") !== -1){
+        showContent();
+    }
+});
+
+
+function hideContent() {
+    $(".container.sidebar-position-left").hide();
+    $(".tpwthwidt").hide();
+    $(".bdshare-slide-button-box.bdshare-slide-style-l5").hide();
+}
+
+function showContent() {
+    $(".container.sidebar-position-left").show();
+    $(".tpwthwidt").show();
+    $(".bdshare-slide-button-box.bdshare-slide-style-l5").show();
+}
